082946b18178174c0ce2d17e1e4e66cb1447f352
Felix Linz <felix.linz@icloud.com>
2023-12-06 21:18:21 +0100

    tag1

---
+++ advent1.py
@@ -0,0 +1,13 @@
+import re
+
+document = "puzzle1.txt"
+secret_number = int()
+
+for line in open(document, "r"):
+    linecharacters = re.findall(r'\d', line)
+    secret_number += int(f"{linecharacters[0]}{linecharacters[-1]}") 
+    
+print(secret_number)
+            
+    
+        
